98325f2497e65387eabbf60d952c00addc31f94e
Deniz Saglam <dnzsaglam@gmail.com>
2020-12-20 17:54:19 +0300

    Final part

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -8,7 +8,7 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
   hero: Hero;
@@ -25,10 +25,16 @@ export class HeroDetailComponent implements OnInit {
 
   getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,6 +1,5 @@
-import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
-import { MessageService } from './../message.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
@@ -8,7 +7,10 @@ import { MessageService } from './../message.service';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
+
   constructor(public messageService: MessageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+  }
+
 }
